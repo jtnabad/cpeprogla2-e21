48a7c499811a5e4d9b7c3c5dc9b20636ecc8fa90
John Tristan Abad <jtnabad01@outlook.com>
2016-09-22 17:09:00 +0800

    Add files via upload

---
+++ hashmat.cpp
@@ -0,0 +1,26 @@
+//abad
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main()
+{
+	int i, a, b, c;
+	cout <<"Enter the number of Soldiers in Hashmat's Army and his opponent's Army: "<<endl;
+
+	for (int i=1; i<=3; i++)
+	{	
+		for(c=1; c<=3; c++)		
+		if(i<=2^36){
+			cin>>a>>b;
+			c= a - b;
+		    cout<<c<<endl;
+						}	
+		
+	}	
+	return 0;
+}	
+
+	
+
